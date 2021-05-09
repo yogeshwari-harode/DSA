8c1aceb3c26e1fda0467642c7e50ae811ccb510f
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-05-09 17:06:07 +0530

    Create Binary Search.cpp

--- Searching/Binary Search.cpp
+++ Searching/Binary Search.cpp
@@ -1,31 +1,37 @@
-//Binary Search
+//BinarySearch
 #include<bits/stdc++.h>
 using namespace std;
 
-bool BinarySearch(vector<int>arr , int val,int n)
+int BinarySearch(vector<int>&arr , int n , int x)
 {
-    int low = 0;
-    int high = n-1;
-    
-    while(low<=high)
+    int l=0;
+    int h=n-1;
+    while(l<=h)
     {
-        int mid = low+(high-low)/2;
-        if(arr[mid]==val)   return true;
-        else if(arr[mid]<val)   low = mid+1;
-        else    high=mid-1;
+        int mid=l+(h-l)/2;
+        if(arr[mid]==x)
+        return mid;
+        else if(arr[mid]<x)
+        l=mid+1;
+        else
+        h=mid;
     }
-    return false;
+    return -1;
 }
 
 int main()
 {
-    int n; cin>>n;
+    int n;    cin>>n;
     vector<int>arr(n);
-    for(int i=0 ; i<n ; i++)    cin>>arr[i];
-    int val; cin>>val;
-    if(BinarySearch(arr,val,n))
-        cout<<"Element found"<<endl;
+    for(int i=0 ; i<n ; i++)
+        cin>>arr[i];
+    int x;  cin>>x;
+        
+    int result = BinarySearch(arr,n,x);
+    if(result==-1)
+    cout<<"Element is not present in the array";
     else
-        cout<<"Element not found"<<endl;
+    cout<<"Element is present at index "<<result;
+    
     return 0;
 }
