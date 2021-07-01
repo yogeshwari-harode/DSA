e078e4363b3824dd85a80630241a09209f093664
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-07-02 00:09:59 +0530

    Create Selection Sort.cpp

--- Sorting/Selection Sort.cpp
+++ Sorting/Selection Sort.cpp
@@ -1,32 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int SelectionSort(vector<int>&arr , int n)
-{
-	for(int i=0 ; i<n ; i++)
-	{
-		int min=i;
-		for(int j=i+1 ; j<n ; j++)
-		{
-		   if(arr[j]<arr[min])
-		    min=j;
-		   swap(arr[i],arr[min]); 
-		}
-	}
+void display(vector<int>&arr, int n) {
+   for(int i = 0; i<n; i++)
+      cout << arr[i] << " ";
+   cout << endl;
 }
 
-void print(vector<int>&arr,int n)
-{
-	for(int i=0 ; i<n ; i++)
-	cout<<arr[i]<<" ";
+void selectionSort(vector<int>&arr, int n) {
+   
+   for(int i = 0; i<n-1; i++) 
+   {
+        int min = i; 
+        for(int j = i+1; j<n; j++)
+         if(arr[j] < arr[min])
+            min = j;
+         swap(arr[i], arr[min]);
+         display(arr, n);
+   }
 }
+int main() {
+   int n;
+   cout << "Enter the size of the Array : ";
+   cin >> n;
+   vector<int>arr(n);          //create an array with given number of elements
+   cout << "Enter elements : " << endl;
+   for(int i = 0; i<n; i++)     cin >> arr[i];
 
-int main()
-{
-	int n;  cin>>n;
-	for(int i=0 ; i<n ; i++)    
-	cin>>arr[i];
-	SelectionSort(arr,n);
-	print(arr,n);
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   selectionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
 return 0;
 }
